26dcd7abc6fe56037f4b3635e1148949fd07d662
Maksim <mshurpo@servicechannel.com>
2015-02-04 18:58:17 +0200

    commit

--- AndroidCafeter/cafeter/src/main/java/com/gsugroup/android/cafeter/activitys/ContentFragmentActivity.java
+++ AndroidCafeter/cafeter/src/main/java/com/gsugroup/android/cafeter/activitys/ContentFragmentActivity.java
@@ -2,12 +2,7 @@ package com.gsugroup.android.cafeter.activitys;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.View;
-
 import com.gsugroup.android.cafeter.CommonFragmentActivity;
 import com.gsugroup.android.cafeter.R;
 import com.gsugroup.android.cafeter.activitys.fragments.ContentFragment;
